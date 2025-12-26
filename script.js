3205428c8cefe8eb4710215edc88fb14f533ff67
edemilsonduartee <edemilson_melo@hotmail.com>
2025-12-26 03:04:11 -0400

    add  gerar somente se o numero for menor que o segundo, e tambem gerar ao clicar na tecla enter

--- script.js
+++ script.js
@@ -3,8 +3,18 @@ function geradorNumeros() {
     const input1 = Math.ceil(document.querySelector('.input1').value)
     const input2 = Math.floor(document.querySelector('.input2').value)
 
-    const result = Math.floor(Math.random() * (input2 - input1 + 1)) + input1;
+    if (input1 >= input2) {
+        alert('O primeiro numero deve ser menor que o segundo')
+        return;
+    }
 
-    alert(result)
+   
+        const result = Math.floor(Math.random() * (input2 - input1 + 1)) + input1;
+        alert(`O numero sorteado foi ${result}`)
+    }
 
-}
+     document.addEventListener('keydown', function (event) {
+        if (event.key === 'Enter') {
+            geradorNumeros();
+        }
+    });
